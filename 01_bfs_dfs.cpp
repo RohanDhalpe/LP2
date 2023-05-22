4566e841274c4bf28af1285c7cbd1224bf00e2ca
Rohan Dhalpe <90591344+RohanDhalpe@users.noreply.github.com>
2023-05-22 23:52:54 +0530

    Update 01_bfs_dfs.cpp

--- 01_bfs_dfs.cpp
+++ 01_bfs_dfs.cpp
@@ -1,73 +1,73 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void bfsTraversal( map<int,list<int>>adjList,vector<int> &visited,int node)
-{
-    visited[node]=1;
-    queue<int>q;
+void bfsTraversal(vector<int> adjList[],vector<int> &visited,int node){
+    queue<int> q;
     q.push(node);
 
-    while(!q.empty())
-    { 
-        int temp=q.front();
+    
+    visited[node] = 1;
+
+    while(!q.empty()){
+        int temp = q.front();
         cout<<temp<<" ";
         q.pop();
 
-        for(auto it:adjList[temp]){
-            if(!visited[it])
-            {
-               q.push(it);
-               visited[it]=1;
+        for(auto j:adjList[temp]){
+            if(visited[j] == 0){
+                q.push(j);
+                visited[j] = 1;
             }
         }
     }
 }
-void dfsTraversal( map<int,list<int>>adjList,vector<int> &visited,int node)
-{   
-   visited[node]=1;
-   cout<<node<<" ";
-   for(auto it:adjList[node])
-   {
-      if(!visited[it]){
-        dfsTraversal(adjList,visited,it);
-      }
-   }
 
-}
-
-int main(){
-
-    map<int,list<int>>adjList;
-
-    int v,e;
-    cout<<"Enter the number of vertices: ";
-    cin>>v;
-
-    cout<<"Enter the number of edges ";
-    cin>>e;
-
-    for(int i=0;i<e;i++)
-    {   
-        int u,v;
-        cin>>u>>v;
-        adjList[u].push_back(v);
-        adjList[v].push_back(u);
-    }
-
-    vector<int>visited(v,0);
-    cout<<"BFS TRAVERSAL IS : ";
-    for(int i=1;i<=v;i++){
-        if(!visited[i]){
-            bfsTraversal(adjList,visited,i);
-        }
-    }
-
-    vector<int>visited(v,0);
-    cout<<"DFS TRAVERSAL IS : ";
-    for(int i=1;i<=v;i++){
+void dfsTraversal(vector<int> adjList[],vector<int> & visited,int node){
+    visited[node] = 1;
+    cout<<node<<" ";
+    for(auto i: adjList[node]){
         if(!visited[i]){
             dfsTraversal(adjList,visited,i);
         }
     }
-    return 0;
+}
+
+int main(){
+    // take input for graph
+    int v,e;
+    cout<<"Enter Number of Vertices : ";
+    cin>>v;
+
+    cout<<"Enter Number of edges : ";
+    cin>>e;
+
+    vector<int> adjList[v];
+
+    int p,q;
+    for(int i = 0; i<e; i++){
+        cin>>p>>q;
+        adjList[p].push_back(q);
+        adjList[q].push_back(p);
+    }
+
+
+    vector<int> visited(v,0);
+    cout<<"BFS Traversal : ";
+    for(int i = 0; i<v; i++){
+        if(visited[i] == 0){
+            bfsTraversal(adjList,visited,i);
+        }
+    }
+    cout<<endl;
+
+    vector<int> visited_(v,0);
+    cout<<"DFS Traversal : ";
+    for(int i = 0; i<v; i++){
+        if(visited_[i] == 0){
+            dfsTraversal(adjList,visited_,i);
+        }
+    }
+    cout<<endl;
+
+ return 0;
 }
